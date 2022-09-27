04809adc4cfcbfa16442f5ed01c9b31390dcc17b
alejandro <cesar.alejandro.fernandez.7@gmail.com>
2022-09-27 12:38:34 -0300

    added isbn attribute

--- test/src/main/java/ar/alexdev/codoacodo/test/domain/Book.java
+++ test/src/main/java/ar/alexdev/codoacodo/test/domain/Book.java
@@ -1,20 +1,27 @@
 package ar.alexdev.codoacodo.test.domain;
 
 public class Book {
+
+    private String isbn;
     private String title;
     private String author;
     private String editorial;
 
     
 
-    public Book(String title, String author, String editorial) {
+    public Book(String isbn, String title, String author, String editorial) {
+        this.isbn = isbn;
         this.title = title;
         this.author = author;
         this.editorial = editorial;
     }
 
-    public static Book newBook(String title, String author, String editorial) {
-        return new Book(title, author, editorial);
+    public static Book newBook(String isbn, String title, String author, String editorial) {
+        return new Book(isbn, title, author, editorial);
+    }
+
+    public String getIsbn() {
+        return isbn;
     }
 
     public String getTitle() {
@@ -28,10 +35,9 @@ public class Book {
     public String getEditorial() {
         return editorial;
     }
-    
+
     @Override
     public String toString() {
-        return "Book [author=" + author + ", editorial=" + editorial + ", title=" + title + "]";
-    }
-
+        return "ISBN= " + isbn + ", TITLE= " + title +", AUTHOR= " + author + ", EDITORIAL= " + editorial;
+    }       
 }
